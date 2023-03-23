bba7a363fed45b0ee83d2c7f4df89f73b27044a0
NikitaLev <nikita.leventev97@gmail.com>
2023-03-23 11:32:35 +0300

    Add TTS

--- STT.py
+++ STT.py
@@ -5,29 +5,18 @@ from glob import glob
 
 from silero.utils import *
 
+device = torch.device('cpu')
+local_file = 'en_v6.jit'
+
+model = torch.jit.load(local_file, map_location=device)
+model.eval()
+decoder = Decoder(model.labels)
 
 def silero_stt_test(name):
-    """    language = 'ru'
-    model_id = 'v5'
-    device = torch.device('cpu')  # gpu also works, but our models are fast enough for CPU
-    model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                           model='silero_stt',
-                                           language='en',  # also available 'de', 'es'
-                                           device=device,
-                                           sp=model_id)"""
 
-    device = torch.device('cpu')
-    local_file = 'en_v6.jit'
-
-    model = torch.jit.load(local_file, map_location=device)
-    model.eval()
-    decoder = Decoder(model.labels)
-
-    # model, decoder, utils = torch.package.PackageImporter(local_file).load_pickle("stt_models", "model")
-
-    # download a single file in any format compatible with TorchAudio
     test_files = glob(name)
     batches = split_into_batches(test_files, batch_size=10)
+
     input = prepare_model_input(read_batch(batches[0]),
                                 device=device)
 
---
+++ TTS.py
@@ -0,0 +1,27 @@
+import torch
+import sounddevice as sd
+import time
+
+import os
+import torch
+
+device = torch.device('cpu')
+torch.set_num_threads(4)
+local_file = 'model_en.pt'
+if not os.path.isfile(local_file):
+    torch.hub.download_url_to_file('https://models.silero.ai/models/tts/en/v3_en.pt',
+                                   local_file)
+model = torch.package.PackageImporter(local_file).load_pickle("tts_models", "model")
+model.to(device)
+
+sample_rate = 48000
+speaker = 'en_0'
+
+
+def test_en(example_text=''):
+
+    audio_paths = model.save_wav(text=example_text,
+                                 speaker=speaker,
+                                 sample_rate=sample_rate
+                                 )
+    # print(audio_paths)
